3a8e6c85a9327344af2747ba521385fa17bbf951
dagu <243065157@qq.com>
2026-03-30 15:22:42 +0800

    fix: remove literal newline tokens in hack.cpp include block

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -13,7 +13,8 @@
 #include <fcntl.h>
 #include <fstream>
 #include <string>
-#include <sys/mman.h>`r`n#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <thread>
 #include <unistd.h>
 
@@ -254,4 +255,5 @@ void hack_prepare(const char *game_data_dir, void *data, size_t length) {
     LOGI("lua dump hook thread start pid=%d", getpid());
     run_lua_dump_hook();
 }
+
 
